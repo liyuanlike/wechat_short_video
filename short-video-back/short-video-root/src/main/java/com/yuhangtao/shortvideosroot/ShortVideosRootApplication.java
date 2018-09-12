d9d2b9c5425bea7190296bdff5538f92928292e7
yuhang Tao <taoyuh1011@foxmail.com>
2018-09-12 20:41:16 +0800

    start back-end

---
+++ short-video-back/short-video-root/src/main/java/com/yuhangtao/shortvideosroot/ShortVideosRootApplication.java
@@ -0,0 +1,12 @@
+package com.yuhangtao.shortvideosroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortVideosRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortVideosRootApplication.class, args);
+    }
+}
